7c7e4e25c275f3fa9fda327aaaa28f821095382e
yashpuneet <yash.puneet02@gmail.com>
2021-02-08 14:20:09 +0800

    Added Two JLabels

--- code/javaGUI.java
+++ code/javaGUI.java
@@ -11,13 +11,22 @@ public class javaGUI
 		//Create new text field
 		final JTextField text = new JTextField();
 		//Set the x axis, y axis, width and height
-		text.setBounds(50, 50, 150, 20);
+		text.setBounds(130, 50, 150, 20);
 
 		//Create a new Button
 		JButton button = new JButton("Click");
 		//Set the x axis, y axis, width and height
 		button.setBounds(130, 100, 100, 40);
 	
+		//Create new labels
+		JLabel label1, label2;
+
+		label1 = new JLabel("First Label");
+		label1.setBounds(10, 50, 100, 30);
+
+		label2 = new JLabel("Second Label");
+		label2.setBounds(10, 100, 100, 30);
+
 		//Create and initialize action listener
 		button.addActionListener(new ActionListener()
 		{
@@ -30,6 +39,8 @@ public class javaGUI
 		//Add components to frame
 		frame.add(button);
 		frame.add(text);
+		frame.add(label1);
+		frame.add(label2);
 
 		//Set frame parameters
 		frame.setSize(400, 500); //Width and Height
